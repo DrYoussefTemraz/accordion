dbf82686964e41c72bf0988cb6330e5db8b610c6
DrYoussefTemraz <youseftahertemraz@gmail.com>
2024-07-03 02:04:33 +0300

    Random color generator for hex and rgb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Accordian from "./Components/Accordian";
+import RandomColor from "./Components/random-color";
+// import Accordian from "./Components/Accordian";
 
 export default function App() {
   return (
     <div>
-      <Accordian />
+      {/* <Accordian /> */}
+      <RandomColor />
     </div>
   );
 }
